c55e9cde7c0815f36612bb904c32a653a50ac26d
MapeMC <michaellipe@hotmail.com>
2024-11-04 15:24:51 -0500

    NavMeshPackage

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
