a04a45014ba1f1aaa609bf65db126850508ea5ad
Daniel Ruiz <danielruiz2009@hotmail.com>
2024-11-06 07:35:28 -0500

    dracula now working since previous commit failed and i lost the changes

---
+++ Assets/Scripts/BossScripts/DraculaScripts/DraculaAI.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraculaAI : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float detectionRange = 20.0f;
+
+    private Transform player;
+    private Animator animator;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            float distance = Vector2.Distance(transform.position, player.position);
+
+            if (distance <= detectionRange)
+            {
+                animator.SetBool("isMoving", true);
+
+                Vector2 direction = (player.position - transform.position).normalized;
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+
+                // Actualiza los parámetros MovementX y MovementY en el Animator
+                animator.SetFloat("MovementX", direction.x);
+                animator.SetFloat("MovementY", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
+    }
+}
